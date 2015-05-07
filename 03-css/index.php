a9a89429de5031073f139bd5e009463e00ff56ce
kapincho <kalin_miloev@abv.bg>
2015-05-07 16:55:53 +0300

    css

---
+++ 03-css/index.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Kalin</title>
+        <link rel="stylesheet" type="text/css" href="img.css" />
+    </head>
+    <body>
+		<section id="content">
+			<div class="img">
+				<a target="_blank" href="/gallery/vik.JPG">
+					<img src="/gallery/vik.JPG" alt="vik.JPG" />
+				</a>
+				<div class="desc">ВиК услуги</div>
+			</div>
+			<div class="img1">
+				<a target="_blank" href="/gallery/horizontalen_shtrang.JPG">
+					<img src="/gallery/horizontalen_shtrang.JPG" alt="horizontalen_shtrang.JPG" />
+				</a>
+				<div class="desc1">хоризонтален щранг</div>
+			</div>
+			<div class="clearfix"></div>
+        </section>
+	</body>
+</html>
