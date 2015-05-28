b41dbe2fd424891230c51766814791849ac5ca87
kapincho <kalin_miloev@abv.bg>
2015-05-28 13:01:30 +0300

    homework 5-9

---
+++ homework/homework6/8/block.php
@@ -0,0 +1,17 @@
+﻿<?php
+function printheader() {
+	echo "<!DOCTYPE html>
+		<html>
+			<head lang=\"en\">
+			<meta charset=\"UTF-8\">
+			<title>Kalin</title>
+			<link rel=\"stylesheet\" href=\"style.css\">
+			</head>
+			<body>";
+}
+
+function printfooter() {
+	echo "</body>
+		</html>";
+}
+?>
